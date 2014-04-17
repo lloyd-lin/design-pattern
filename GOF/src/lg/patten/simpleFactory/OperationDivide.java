cf1c2be774ada17c13e5b4c05a3ea631b12698cc
lloyd-lin <frankgts@hotmail.com>
2014-04-17 16:20:49 +0800

    improve divide

--- GOF/src/lg/patten/simpleFactory/OperationDivide.java
+++ GOF/src/lg/patten/simpleFactory/OperationDivide.java
@@ -1,10 +1,13 @@
 package lg.patten.simpleFactory;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 public class OperationDivide extends Operation{
 
 	public BigDecimal getResult(){
-		return super.getNum1().divide(super.getNum2()).setScale(4);
+		MathContext mc = new MathContext(2, RoundingMode.HALF_DOWN);
+		return super.getNum1().divide(super.getNum2(),mc);
 	}
 }
